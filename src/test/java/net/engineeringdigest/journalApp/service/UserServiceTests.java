20a10f99ac78e70fe3f25389d57278448a52b921
Utpalkumar53 <utpalkumarkashyap53@gmail.com>
2026-04-09 00:05:56 +0530

    Fix Mockito annotations in UserServiceTests

--- src/test/java/net/engineeringdigest/journalApp/service/UserServiceTests.java
+++ src/test/java/net/engineeringdigest/journalApp/service/UserServiceTests.java
@@ -3,22 +3,26 @@ package net.engineeringdigest.journalApp.service;
 import net.engineeringdigest.journalApp.entity.User;
 import net.engineeringdigest.journalApp.repository.UserRepository;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ArgumentsSource;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+//@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class UserServiceTests {
 
-    @Autowired
+    @Mock
     private UserRepository userRepository;
 
-    @Autowired
+    @Mock
     private UserService userService;
 
 //    @BeforeAll
